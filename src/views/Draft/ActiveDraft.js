cdbab0729d8ea7f898a7946b7d70045ec67cadf2
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-05 14:30:13 -0600

    added views and created public and private cards for teams and leagues.

---
+++ src/views/Draft/ActiveDraft.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ActiveDraft extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Active Draft</h1>
+      </div>
+    )
+  }
+}
---
+++ src/views/Draft/InputCaptainNames.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function InputCaptainNames() {
+  return (
+    <div>
+      <h1>Input captain's names</h1>
+    </div>
+  )
+}
