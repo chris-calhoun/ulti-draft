4665ae618e852232f4397f83e29550bff2a3ec9e
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-07 09:28:39 -0600

    set up google auth.

---
+++ src/components/Auth/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import googleImage from './Sign-in-with-Google.png';
+
+export default class Auth extends Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
+
+  render() {
+    return (
+      <div className='Auth'>
+        <button className='btn btn-secondary' onClick={this.loginClickEvent}>
+          <img src={googleImage} alt='Google Sign In Button' />
+        </button>
+      </div>
+    );
+  }
+}
