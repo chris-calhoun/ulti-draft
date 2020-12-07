8db5a74dc6bc96d7bbe5a327962d1d684989e097
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-07 09:25:04 -0600

    check auth state of user.

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import fbConnection from '../helpers/data/connection';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../helpers/Routes';
 import MyNavbar from '../components/MyNavbar';
 import '../styles/App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: null,
+  };
+
+  componetDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
         <Router>
-          <MyNavbar />
-          <Routes />
+          <MyNavbar authed={authed} />
+          <Routes authed={authed} />
         </Router>
       </div>
     );
