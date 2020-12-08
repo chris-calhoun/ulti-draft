eaef2350a11ac0f7b0fb9e8d28fa8a5a645c2bc6
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-07 21:20:42 -0600

    added buttons and title to home page.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      <h1 className='title mb-5'>ulti.draft</h1>
+      <div className='homeBtns'>
+        <Button className='mx-3' color="success">create draft</Button>{' '}
+        <Button className='mx-3' color="success">join draft</Button>{' '}
+      </div>
     </div>
   );
 }
