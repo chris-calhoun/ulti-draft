22cc0ecb1b4f8e9d1698b6caf519f6850f31421e
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-08 21:48:03 -0600

    comments and changed function names.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import { useHistory } from 'react-router-dom';
+// import AppModal from '../components/Modal';
 
 export default function Home(props) {
   const history = useHistory();
   const navigateToCreate = () => history.push('/create-draft');
 
-  const loginModal = () => {
+  const handleCreateBtnClick = () => {
     // if not logged in, show modal with login button
-    !props.authed ? console.warn('log in please')
-    // else, continue onto create draft
+    !props.authed ? console.warn('log in bish')
+    // else, continue onto create draft view
       : navigateToCreate();
   };
 
@@ -17,8 +18,7 @@ export default function Home(props) {
     <div className='home'>
       <h1 className='title mb-5'>ulti.draft</h1>
       <div className='homeBtns'>
-        <Button onClick={ loginModal } className='draftBtns btn-block' color="success">create draft</Button>
-        {/* <Button tag={Link} to='/create-draft' onClick={ loginModal } className='draftBtns btn-block' color="success">create draft</Button> */}
+        <Button onClick={ handleCreateBtnClick } className='draftBtns btn-block' color="success">create draft</Button>
         <Button className='draftBtns btn-block' color="success">join draft</Button>
       </div>
     </div>
