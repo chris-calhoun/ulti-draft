70cbe3e3d31fc3c6798b7eb35a26f1005148f3b4
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-08 23:21:00 -0600

    fixed errors.

--- src/components/Modal/loginModal.js
+++ src/components/Modal/loginModal.js
@@ -1,12 +1,18 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'reactstrap';
 
 const ModalExample = (props) => {
   const {
     buttonLabel,
-    className
+    className,
   } = props;
 
   const [modal, setModal] = useState(false);
@@ -19,7 +25,7 @@ const ModalExample = (props) => {
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle}>Modal title</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
@@ -28,6 +34,6 @@ const ModalExample = (props) => {
       </Modal>
     </div>
   );
-}
+};
 
 export default ModalExample;
