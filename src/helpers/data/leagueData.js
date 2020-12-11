9d8d5d27b1baa164fe61019053f371194db40fc4
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-10 19:00:31 -0600

    fixed errors.

--- src/helpers/data/leagueData.js
+++ src/helpers/data/leagueData.js
@@ -5,8 +5,9 @@ const baseUrl = 'https://ulti-draft-default-rtdb.firebaseio.com/';
 const createLeague = (object) => new Promise((resolve, reject) => {
   axios.post(`${baseUrl}/League.json`, object)
     .then((response) => {
-      axios.patch(`${baseUrl}/League/${response.data.name}.json`, {firebaseKey: response.data.name }).then(resolve);
+      axios.patch(`${baseUrl}/League/${response.data.name}.json`, { firebaseKey: response.data.name }).then(resolve);
     }).catch((error) => reject(error));
 });
 
+// eslint-disable-next-line
 export default { createLeague };
