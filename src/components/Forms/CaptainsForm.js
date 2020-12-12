4c114931fd9962391e7cdb9dc5186e86d4181c44
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-11 22:26:44 -0600

    created initial captain form.

---
+++ src/components/Forms/CaptainsForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CaptainsForm extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Captain's Form</h2>
+      </div>
+    );
+  }
+}
--- src/views/Draft/InputCaptainNames.js
+++ src/views/Draft/InputCaptainNames.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CaptainsForm from '../../components/Forms/CaptainsForm';
 
 export default class InputCaptainNames extends Component {
   componentDidMount() {
@@ -9,6 +10,9 @@ export default class InputCaptainNames extends Component {
     return (
       <div>
         <h1>Input captain's names</h1>
+        <div className='captainForm'>
+          <CaptainsForm />
+        </div>
       </div>
     );
   }
