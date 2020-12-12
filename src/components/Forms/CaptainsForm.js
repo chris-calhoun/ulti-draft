73db91814ae9eda5c3eccfe0e8b16b3f6d32214b
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-11 23:44:10 -0600

    working on captain form.

--- src/components/Forms/CaptainsForm.js
+++ src/components/Forms/CaptainsForm.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+} from 'reactstrap';
 
 export default class CaptainsForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.warn('submitted');
+  }
+
   render() {
     return (
       <div>
         <h2>Captain's Form</h2>
+        <p>Props: {this.props.numTeams}</p>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup>
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       </div>
     );
   }
--- src/views/Draft/InputCaptainNames.js
+++ src/views/Draft/InputCaptainNames.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import CaptainsForm from '../../components/Forms/CaptainsForm';
 
 export default class InputCaptainNames extends Component {
-  componentDidMount() {
-    console.warn('number of teams: ', this.props.location.state.numTeams);
-  }
-
   render() {
     return (
       <div>
-        <h1>Input captain's names</h1>
+        <h1>Captain Names</h1>
         <div className='captainForm'>
-          <CaptainsForm />
+          <CaptainsForm numTeams={this.props.location.state.numTeams}/>
         </div>
       </div>
     );
