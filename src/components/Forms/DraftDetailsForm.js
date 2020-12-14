826f5b6b06a94973908f8331fa30ef5969a52e2d
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-13 21:16:51 -0600

    all players input correctly.

--- src/components/Forms/DraftDetailsForm.js
+++ src/components/Forms/DraftDetailsForm.js
@@ -74,8 +74,14 @@ class DraftDetailsForm extends Component {
     const { playerData } = this.state;
     LeagueData.createLeague(this.state.leagueObj).then((response) => {
       // console.warn(response.data.firebaseKey);
+
+      // this is definitely not the most elegant way to handle this problem
+      // for (let i = 0; i < playerData.length; i + 1) {
+      //   PlayerData.createPlayer(playerData);
+      // }
+
       playerData.forEach((player) => {
-        PlayerData.createPlayer(playerData);
+        PlayerData.createPlayer(player);
       });
       this.navigateToCaptainsForm(response.data.firebaseKey);
     });
--- src/helpers/data/playerData.js
+++ src/helpers/data/playerData.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 const baseUrl = 'https://ulti-draft-default-rtdb.firebaseio.com/';
 
 const createPlayer = (obj) => new Promise((resolve, reject) => {
-  // console.warn(arrOfObjs);
-  // arrOfObjs.forEach((team) => {
-  // console.warn(team);
-  axios.post(`${baseUrl}/Player.json`, obj['1'].data)
+  axios.post(`${baseUrl}/Player.json`, obj.data)
     .then((response) => {
       axios.patch(`${baseUrl}/Player/${response.data.name}.json`, { id: response.data.name }).then(resolve);
     }).catch((error) => reject(error));
