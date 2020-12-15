e76ad285d6e8c15ad199aac77db87f4b638d9d9f
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-14 20:19:52 -0600

    STATE UPDATES IN REALTIME.

--- src/views/Draft/ActiveDraft.js
+++ src/views/Draft/ActiveDraft.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { base } from '../../helpers/data/connection';
 
 export default class ActiveDraft extends Component {
 state = {
   draftCode: '',
+  players: {},
 }
 
 componentDidMount() {
   const draftCode = this.props.match.params.id;
+  this.ref = base.syncState('/Player', {
+    context: this,
+    state: 'players',
+  });
+
   this.setState({
     draftCode,
   });
