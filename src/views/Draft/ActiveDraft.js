994162ef4eb03fe1f12275f6ec72e878c9fb3024
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-14 23:10:32 -0600

    added static table.

--- src/views/Draft/ActiveDraft.js
+++ src/views/Draft/ActiveDraft.js
@@ -1,6 +1,10 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 import React, { Component } from 'react';
+import {
+  Table,
+  Button,
+} from 'reactstrap';
 
 export default class ActiveDraft extends Component {
 state = {
@@ -29,7 +33,27 @@ render() {
   return (
     <div>
       <h1>Active Draft</h1>
-      <p>DraftCode: {this.state.draftCode}</p>
+      <p>Draft Code: {this.state.draftCode}</p>
+      <Table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+          <th>Add Player</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>25</td>
+          <td>M</td>
+          <Button>Add</Button>
+        </tr>
+      </tbody>
+    </Table>
     </div>
   );
 }
