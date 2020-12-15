25e4b7e9d125db56b2d91a278743062cc893d696
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-15 00:04:07 -0600

    populated table with league data and styled.

--- src/views/Draft/ActiveDraft.js
+++ src/views/Draft/ActiveDraft.js
@@ -34,26 +34,32 @@ render() {
     <div>
       <h1>Active Draft</h1>
       <p>Draft Code: {this.state.draftCode}</p>
-      <Table>
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Age</th>
-          <th>Gender</th>
-          <th>Add Player</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>25</td>
-          <td>M</td>
-          <Button>Add</Button>
-        </tr>
-      </tbody>
-    </Table>
+      <div class="d-flex justify-content-center mx-5 my-5">
+        <Table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Add Player</th>
+          </tr>
+        </thead>
+        <tbody>
+            <>
+            {Object.values(this.state.players).map((player) => (
+              <tr>
+                <td>{player.first_name}</td>
+                <td>{player.last_name}</td>
+                <td>{player.age}</td>
+                <td>{player.gender}</td>
+                <td><Button>Add</Button></td>
+              </tr>
+            ))}
+            </>
+        </tbody>
+      </Table>
+    </div>
     </div>
   );
 }
