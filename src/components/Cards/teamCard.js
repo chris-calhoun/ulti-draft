b80b8ca7810ce613cf5d0b5a286c5a2b5248adc8
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-15 20:23:00 -0600

    added modal to team cards to view players.

--- src/components/Cards/teamCard.js
+++ src/components/Cards/teamCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AppModal from '../Modal/appModal';
 
 export default function TeamCard({ team }) {
   return (
     <div className='team-card card m-2'>
-      <h5 className='card-title'>Captain: {team.teamCaptain}</h5>
-      {/* <div className='card-body'></div> */}
+      <div className='card-body'>
+        <h5 className='card-title'>Captain: {team.teamCaptain}</h5>
+        <AppModal title={'Team Players'} buttonLabel={'View Players'}></AppModal>
+      </div>
     </div>
   );
 }
--- src/views/Teams.js
+++ src/views/Teams.js
@@ -23,23 +23,25 @@ export default class Teams extends Component {
     LeagueTeamsData.getLeagueTeams(leagueId).then((response) => {
       const teamArray = [];
       response.forEach((team) => {
-        teamArray.push(TeamData.getTeam(team.leagueId));
+        // console.warn(team.teamKey);
+        teamArray.push(TeamData.getTeam(team.teamKey));
       });
       return Promise.all(teamArray);
     })
   )
 
   render() {
-    // const { teams } = this.state;
-    // const renderTeams = () => (
-    //   teams.map((team) => (
-    //     <TeamCard key={team.teamKey} team={team} />
-    //   ))
-    // );
+    const { teams } = this.state;
+    const renderTeams = () => (
+      teams.map((team) => (
+        <TeamCard key={team.teamKey} team={team} />
+      ))
+    );
     return (
       <div>
         <h1>Teams</h1>
         <div className='d-flex flex-wrap container'>
+          {renderTeams()}
         </div>
       </div>
     );
