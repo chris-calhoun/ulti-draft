d6786305d642efcd8bbe630c34f726d23cc3c603
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-12-18 23:33:39 -0600

    minor style changes.

--- src/components/Cards/teamCard.js
+++ src/components/Cards/teamCard.js
@@ -5,7 +5,8 @@ export default function TeamCard({ team }) {
   return (
     <div className='team-card card m-2'>
       <div className='card-body'>
-        <h5 className='card-title'>Captain: {team.teamCaptain}</h5>
+        <h5 className='card-title'>Team Captain</h5>
+        <p>{team.teamCaptain}</p>
         <Link className='btn btn-primary' color="success" to={`/players/${team.firebaseKey}`}>
           View Players
         </Link>
